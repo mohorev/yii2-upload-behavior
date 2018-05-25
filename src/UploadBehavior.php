ae818da75ad7d0cffc499ce1790c9b5971d91d07
Max Alexandrov <max@7u3.ru>
2018-05-26 00:13:23 +0300

    Fix.

--- src/UploadBehavior.php
+++ src/UploadBehavior.php
@@ -311,8 +311,8 @@ class UploadBehavior extends Behavior
         } elseif (is_callable($path) || is_array($path)) {
             return call_user_func($path, $model);
         } else {
-            throw new InvalidConfigException(
-                '`path` property must be string or array or callable: ' . gettype($path) . ' given.'
+            throw new InvalidArgumentException(
+                '$path argument must be a string, array or callable: ' . gettype($path) . ' given.'
             );
         }
     }
